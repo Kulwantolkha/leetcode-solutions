62a4963c53254fc9f13698f6be224866f252b2eb
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2024-07-29 23:35:09 +0530

    Time: 469 ms (5.02%), Space: 16.7 MB (18.83%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.py
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.py
@@ -1,7 +1,15 @@
 class Solution:
     def makeSmallestPalindrome(self, s: str) -> str:
-        letters = list(s)
-        for i in range(len(s) // 2):
-            letters[i] = letters[~i] = min(letters[i], letters[~i])
+        for i in range(len(s)):
+            if(s[i]==s[len(s)-i-1]):
+                continue
+            else:
+                a=min(s[i],s[len(s)-i-1])
+                b = max(s[i],s[len(s)-i-1])
+                if(a==s[len(s)-i-1]):
+                    s=s[:i]+b+s[i+1:]
+                else:
+                    s=s[:(len(s)-1-i)]+a+s[((len(s)-1-i)+1):]
+        return s
 
-        return ''.join(letters)
+        
