db27f7707e2998e5204827630b90c541487fe557
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2024-10-15 19:12:17 +0530

    Time: 7 ms (71.12%), Space: 21.6 MB (11.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> st;
-        for(auto it=nums.begin();it!=nums.end();it++){
-            st.insert(*it);
+        // set<int> st;
+        // for(auto it=nums.begin();it!=nums.end();it++){
+        //     st.insert(*it);
+        // }
+        // nums.clear();
+        // for(auto it:st){
+        //     nums.push_back(it);
+        // }
+        // return st.size(); 
+//         int i=0;
+//         for(auto j=nums.begin()+1;j!=nums.end();j++){
+//             if(*j!=nums[i]){
+//                 nums[i+1]=*j;
+//                 i++;
+//             }
+//         }
+//         return nums.size();
+        vector<int> v;
+        int j=0;
+        v.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(v[j]!=nums[i]){
+                v.push_back(nums[i]);
+                j++;
+            }
         }
-        nums.clear();
-        for(auto it:st){
-            nums.push_back(it);
-        }
-        return st.size(); 
+        // nums.clear();
+        nums = v;
+        return nums.size();
     }
 };
