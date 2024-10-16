2071a6513a5fbd708c4672655e27094b134dcf74
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2024-10-16 09:24:50 +0530

    Time: 14 ms (72.13%), Space: 21.9 MB (72.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        //brute force approach
+        /*
         vector<int> v;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
@@ -11,5 +13,20 @@ public:
             v.push_back(0);
         }
         nums=v;
+        */
+        //more optimal approach
+        int j=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        for(int i=j+1;i<nums.size();i++){
+            if(nums[i]!=0 && j>-1){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
     }
 };
