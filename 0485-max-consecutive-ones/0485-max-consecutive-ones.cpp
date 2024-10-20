904ba5c877cf8d091e74e7236a4b00f36302d310
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2024-10-20 18:08:06 +0530

    Time: 0 ms (100.00%), Space: 49.2 MB (21.89%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -1,35 +1,50 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int count = 1;
-        vector<int> v;
+        // int count = 1;
+        // vector<int> v;
+        // int max=0;
+        // if(nums.size()==1){
+        //     if(nums[0]==1){
+        //         return 1;
+        //     }
+        //     else{
+        //         return 0;
+        //     }
+        // }
+        // int flag = 0;
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i]==1 || nums[i-1]==1){
+        //         flag=1;
+        //     }
+        //     if(nums[i]==1 && nums[i-1]==1){
+        //         count++;
+        //         v.push_back(count);
+        //     }
+        //     else{
+        //         v.push_back(count);
+        //         count=1;
+        //     }
+        // }
+        // if(flag==0){
+        //     return 0;
+        // }
+        // max = *max_element(v.begin(),v.end());
+        // return max;
+        
         int max=0;
-        if(nums.size()==1){
-            if(nums[0]==1){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-        int flag = 0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==1 || nums[i-1]==1){
-                flag=1;
-            }
-            if(nums[i]==1 && nums[i-1]==1){
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
                 count++;
-                v.push_back(count);
+                if(count>max){
+                    max=count;
+                }
             }
             else{
-                v.push_back(count);
-                count=1;
+                count=0;
             }
         }
-        if(flag==0){
-            return 0;
-        }
-        max = *max_element(v.begin(),v.end());
         return max;
     }
 };
