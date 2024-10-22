13ba30a8dd2c63a4d2d10e0bb8dc95460125baac
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2024-10-22 20:18:26 +0530

    Time: 15 ms (50.97%) | Memory: 16.4 MB (8.03%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // int sum=0;
+        // bool k=false;
+        // vector<int> v;
+        // for(int i=0;i<nums.size();i++){
+        //     sum=nums[i];
+        //     for(int j=i+1;j<nums.size();j++){
+        //         sum+=nums[j];
+        //         if(sum==target){
+        //             k=true;
+        //             v.push_back(i);
+        //             v.push_back(j);
+        //             break;
+        //         }
+        //         else{
+        //             sum=nums[i];
+        //         }
+        //     }
+        //     if(k==true){
+        //         break;
+        //     }
+        // }
+        // return v;
+
+        //Better Solution
+        map<int,int> mpp;
         int sum=0;
-        bool k=false;
         vector<int> v;
         for(int i=0;i<nums.size();i++){
-            sum=nums[i];
-            for(int j=i+1;j<nums.size();j++){
-                sum+=nums[j];
-                if(sum==target){
-                    k=true;
-                    v.push_back(i);
-                    v.push_back(j);
-                    break;
-                }
-                else{
-                    sum=nums[i];
-                }
+            int needed_number = target-nums[i];
+            if(mpp.find(needed_number)!=mpp.end()){
+                v.push_back(mpp[needed_number]);
+                v.push_back(i);
             }
-            if(k==true){
-                break;
-            }
+            mpp[nums[i]]=i;
         }
         return v;
     }
