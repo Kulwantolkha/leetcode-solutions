07c5347124deef4845a87ef33d3b0bdc0a64a44f
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2024-10-28 01:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (49.81%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        int n = nums.size()/2;
-        map<int,int> mpp;
+    // int majorityElement(vector<int>& nums) {
+    //     int n = nums.size()/2;
+    //     map<int,int> mpp;
+    //     for(int i=0;i<nums.size();i++){
+    //         mpp[nums[i]]++;
+    //     }
+    //     int num;
+    //     for(auto it:mpp){
+    //         if(it.second>n){
+    //             return it.first;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    //Moore Voting Algorithm
+    int majorityElement(vector<int>& nums){
+        int cnt=0;
+        int el;
         for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
+          if(cnt==0){
+            cnt=1;
+            el=nums[i];
+          }  
+          else if(nums[i]==el){
+            cnt++;
+          }
+          else{
+            cnt--;
+          }
         }
-        int num;
-        for(auto it:mpp){
-            if(it.second>n){
-                return it.first;
+        int cnt1=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==el){
+                cnt1++;
             }
+        }
+        if(cnt1>nums.size()/2){
+            return el;
         }
         return -1;
     }
