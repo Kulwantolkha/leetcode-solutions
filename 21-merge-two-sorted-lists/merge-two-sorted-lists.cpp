69e8a6c065786f764f465f23686ecc96af067e93
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2025-02-03 17:14:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (29.02%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,35 +10,16 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL) return list2;
-        if(list2==NULL) return list1;
-        ListNode* ptr = list1;
-        if(list1->val > list2->val){
-            ptr = list2;
-            list2 = list2->next;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL)    return l2;
+        if(l2==NULL)    return l1;
+        if(l1->val <= l2 -> val){
+            l1->next = mergeTwoLists(l1->next,l2);
+            return l1;
         }
         else{
-            list1 = list1->next;
+            l2->next = mergeTwoLists(l1,l2->next);
+            return l2;
         }
-        ListNode* curr = ptr;
-        while(list1!=NULL && list2!=NULL){
-            if(list1->val < list2->val){
-                curr->next = list1;
-                list1 = list1->next;
-            }
-            else{
-                curr->next = list2;
-                list2 = list2->next;
-            }
-            curr = curr->next;
-        }
-        if(list1!=NULL){
-            curr->next = list1;
-        }
-        if(list2!=NULL){
-            curr->next = list2;
-        }
-        return ptr;
     }
 };
