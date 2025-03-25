9df51516a1b26ae0aa961e6143eca3c2e572db2b
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2025-03-25 23:34:14 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (5.90%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -34,12 +34,6 @@ public:
         vector<int> ans2;
         if(root->left)    checkTree(root->left,ans1);
         if(root->right)    checkTree1(root->right,ans2);
-        for(int i=0;i<ans1.size();i++){
-            cout<<ans1[i]<<" ";
-        }cout<<endl;
-        for(int i=0;i<ans2.size();i++){
-            cout<<ans2[i]<<" ";
-        }cout<<endl;
         if(ans1==ans2)  return true;
         return false;
     }
