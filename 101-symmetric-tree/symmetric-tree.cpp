d6d5c9abf622255bde3faeff5ac6add8ddb17792
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2025-03-25 23:27:02 +0530

    Time: 17 ms (0.90%) | Memory: 18.7 MB (5.90%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void checkTree(TreeNode* root,vector<int>& ans){
+        if(root==NULL){
+            ans.push_back(-1);  
+            return;
+        }
+        ans.push_back(root->val);
+        checkTree(root->left,ans);
+        checkTree(root->right,ans);
+    }
+    void checkTree1(TreeNode* root,vector<int>& ans){
+        if(root==NULL){
+            ans.push_back(-1);  
+            return;
+        }
+        ans.push_back(root->val);
+        checkTree1(root->right,ans);
+        checkTree1(root->left,ans);
+    }
+    bool isSymmetric(TreeNode* root) {
+        vector<int> ans1;
+        vector<int> ans2;
+        if(root->left)    checkTree(root->left,ans1);
+        if(root->right)    checkTree1(root->right,ans2);
+        for(int i=0;i<ans1.size();i++){
+            cout<<ans1[i]<<" ";
+        }cout<<endl;
+        for(int i=0;i<ans2.size();i++){
+            cout<<ans2[i]<<" ";
+        }cout<<endl;
+        if(ans1==ans2)  return true;
+        return false;
+    }
+};
